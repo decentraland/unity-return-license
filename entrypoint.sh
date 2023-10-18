080aeda039a2a3aed984351657c8e91348b3fa0a
Juan Molteni <juanignaciomolteni@gmail.com>
2023-10-18 08:53:16 -0300

    Trying to do it in Windows

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-#
-# PROFESSIONAL (SERIAL) LICENSE MODE
-#
-# This will return the license that is currently in use.
-#
-unity-editor \
-  -logFile /dev/stdout \
-  -quit \
-  -returnlicense \
-  -username "$UNITY_EMAIL" \
-  -password "$UNITY_PASSWORD" \
-
+if [[ -n "$UNITY_SERIAL" ]]; then
+  #
+  # PROFESSIONAL (SERIAL) LICENSE MODE
+  #
+  # This will return the license that is currently in use.
+  #
+  unity-editor \
+    -logFile /dev/stdout \
+    -quit \
+    -returnlicense
+else
+  echo "No UNITY_SERIAL detected! No license was returned."
+fi
