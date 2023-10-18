19fe9abf91e3c352c2e10c72a4a85d1640c92e16
Juan Molteni <juanignaciomolteni@gmail.com>
2023-10-18 08:34:25 -0300

    Removed previous activation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ -n "$UNITY_SERIAL" ]]; then
-  #
-  # PROFESSIONAL (SERIAL) LICENSE MODE
-  #
-  # This will return the license that is currently in use.
-  #
-  unity-editor \
-    -logFile /dev/stdout \
-    -quit \
-    -returnlicense \
-    -username "$UNITY_EMAIL" \
-    -password "$UNITY_PASSWORD" \
-else
-  echo "No UNITY_SERIAL detected! No license was returned."
-fi
+#
+# PROFESSIONAL (SERIAL) LICENSE MODE
+#
+# This will return the license that is currently in use.
+#
+unity-editor \
+  -logFile /dev/stdout \
+  -quit \
+  -returnlicense \
+  -username "$UNITY_EMAIL" \
+  -password "$UNITY_PASSWORD" \
+
